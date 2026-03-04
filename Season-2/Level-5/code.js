b4f1f5e75cfd1f3a7eced9d530c457fc96ac5e45
Charan_Kuravi <collegemail36@gmail.com>
2026-03-04 18:56:47 +0530

    Refactor SHA1 hash function to use sha1_round

--- Season-2/Level-5/code.js
+++ Season-2/Level-5/code.js
@@ -8,6 +8,8 @@ var CryptoAPI = (function() {
 		b2a: function(b) { }
 	};
 
+	var sha1_round = function(H, w) { };
+
 	var API = {
 		sha1: {
 			name: 'sha1',
@@ -15,6 +17,7 @@ var CryptoAPI = (function() {
 			size: 20,
 			block: 64,
 			hash: function(s) {
+				s = String(s);
 				var len = (s += '\x80').length,
 					blocks = len >> 6,
 					chunk = len & 63,
@@ -22,8 +25,8 @@ var CryptoAPI = (function() {
 					i = 0,
 					j = 0,
 					H = [0x67452301, 0xEFCDAB89, 0x98BADCFE, 0x10325476, 0xC3D2E1F0],
-					w = [];
-					
+					w = Object.create(null);
+
 				while (chunk++ != 56) {
 					s += "\x00";
 					if (chunk == 64) {
@@ -31,26 +34,25 @@ var CryptoAPI = (function() {
 						chunk = 0;
 					}
 				}
-				
+
 				for (s += "\x00\x00\x00\x00", chunk = 3, len = 8 * (len - 1); chunk >= 0; chunk--) {
 					s += encoding.b2a(len >> (8 * chunk) & 255);
 				}
-					
+
 				for (i = 0; i < s.length; i++) {
 					j = (j << 8) + encoding.a2b(s[i]);
 					if ((i & 3) == 3) {
 						w[(i >> 2) & 15] = j;
 						j = 0;
 					}
-					if ((i & 63) == 63) CryptoAPI.sha1._round(H, w);
+					if ((i & 63) == 63) sha1_round(H, w);
 				}
-				
+
 				for (i = 0; i < H.length; i++)
 					for (j = 3; j >= 0; j--)
 						res += encoding.b2a(H[i] >> (8 * j) & 255);
 				return res;
 			}, // End "hash"
-			_round: function(H, w) { }
 		} // End "sha1"
 	}; // End "API"
 
